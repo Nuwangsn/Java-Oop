ad6fd90e86d1609a69bafdfd6d275a63177d2e7d
Nuwangsn <nuwansameeragsn@gmail.com>
2023-06-29 20:59:33 +0530

    Add changes to Earth, Human class

---
+++ src/OOP/Earth.java
@@ -0,0 +1,9 @@
+package OOP;
+
+public class Earth {
+    public static void main(String[] args) {
+        System.out.println("------ The Earth is Born Now -----");
+        Human human1 = new Human("Nuwan Sameera",26,56,"Black");
+
+    }
+}
--- src/OOP/Human.java
+++ src/OOP/Human.java
@@ -2,7 +2,26 @@ package OOP;
 
 public class Human {
 
-    public void speak (){
-        System.out.println("human is speaking");
+    String name;
+    int age;
+    int heightInInches;
+    String eyeColor;
+
+    public Human(String name,int age,int heightInInches,String eyeColor){
+        this.name = name;
+        this.age = age;
+        this.heightInInches=heightInInches;
+        this.eyeColor=eyeColor;
+        System.out.println("Human is landing on the Earth to create the World");
+        speak(name,age,heightInInches,eyeColor);
     }
+
+    public void speak (String name,int age,int heightInInches,String eyeColor){
+        System.out.println("Hi My name is "+name+" good to meet you.");
+        System.out.println("My age is "+age);
+        System.out.println("I am "+heightInInches+" inches tall");
+        System.out.println("My eye are "+eyeColor+" color");
+    }
+
+
 }
